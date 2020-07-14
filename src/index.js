5f84d5e64b20dab4e5f25d469a1d4b6d84429381
Stama Eleazar <eleoatam10@gmail.com>
2020-07-14 16:27:40 -0500

    API call using axios returned data

--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 // import {shout} from "./test" //1
-import './styles/main.css'
-import "./images/favicon.png"
 // import string from "./models/Search" //2
 // import {add, multiply, ID} from "./views/searchView"//3A
-import {add as a, multiply as m} from "./views/searchView" //3B
-import * as searchView from "./views/searchView" //3C
+// import {add as a, multiply as m} from "./views/searchView" //3B
+// import * as searchView from "./views/searchView" //3C
 
 
 // console.log(shout('John')) //1
 // console.log(string)     //2
 // console.log(`to add: ${add(3, 4)} and to multipply: ${multiply(4, 5)} and my id is: ${ID}`)//3A
-console.log(`${a(3, 4)} and ${m(3, 4)}`) //3B
-console.log(`${searchView.add(3, 4)}`) //3C
+// console.log(`${a(3, 4)} and ${m(3, 4)}`) //3B
+// console.log(`${searchView.add(3, 4)}`) //3C
 
 
+///Start of project ->
 
+import './styles/main.css'
+import "./images/favicon.png"
+
+import axios from "axios";
+
+
+//API Call
+async function getResults(mySearch){
+    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${mySearch}`);
+    console.log(res)
+    const recipes = await res.data.recipes
+    console.log(recipes)
+
+}
+
+getResults('pizza')
 
 
 
