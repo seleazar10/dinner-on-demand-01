fab5f782aae3bc2f6f25d80e438d42d1c9add003
Stama Eleazar <eleoatam10@gmail.com>
2020-07-15 12:31:18 -0500

    state in place, search controller working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,74 @@
+import './styles/main.css'
+import "./images/favicon.png"
+import Search from "./models/Search";
+
+
+/*
+Global state of the app
+- Search Object
+- Current recipe object
+- Shoping list object
+- Liked recipes
+
+
+*/
+
+const state = {};
+
+const controlSearch = async () => {
+
+    // 1. get search from view
+    const mySearch = document.querySelector('.search__field').value
+    // const mySearch = 'pizza'
+    console.log(mySearch)
+
+    if (mySearch) {
+
+        //2. new search object and add to state
+        state.search = new Search(mySearch)
+        console.log(state.search)
+
+        //3. Prepare UI for result
+
+
+        //4. Search for recipes
+        await state.search.getResults();
+
+
+        //5. Render result in UI
+        console.log("--------------------")
+
+        console.log(state.search.result)
+
+    }else{
+        console.log('problem')
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', (e) => {
+    e.preventDefault()
+    controlSearch()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///======================PRACTICE=================================//////////////
+
+
 // import {shout} from "./test" //1
 // import string from "./models/Search" //2
 // import {add, multiply, ID} from "./views/searchView"//3A
@@ -11,25 +82,4 @@
 // console.log(`${a(3, 4)} and ${m(3, 4)}`) //3B
 // console.log(`${searchView.add(3, 4)}`) //3C
 
-
-///Start of project ->
-
-import './styles/main.css'
-import "./images/favicon.png"
-
-import axios from "axios";
-
-
-//API Call
-async function getResults(mySearch){
-    const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${mySearch}`);
-    console.log(res)
-    const recipes = await res.data.recipes
-    console.log(recipes)
-
-}
-
-getResults('pizza')
-
-
-
+///======================PRACTICE=================================//////////////
--- src/models/Search.js
+++ src/models/Search.js
@@ -1,3 +1,36 @@
-// export default "I am an imported text/string";
+import axios from "axios";
 
-export
+
+export default class Search {
+    constructor(mySearch) {
+        this.mySearch = mySearch;
+    }
+
+
+    //API Call
+    async getResults() {
+
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.mySearch}`);
+            console.log(res) //jSON returned
+            this.allRecipes = await res.data.recipes
+            console.log(this.allRecipes) //recipe list - objects
+            console.log('no error')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+
+
+
+
+
+///======================PRACTICE=================================//////////////
+// export default "I am an imported text/string";
+///======================PRACTICE=================================//////////////
+
+
+
+
